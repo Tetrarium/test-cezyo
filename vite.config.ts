6e8917d7d835783b5b14d2d04a2c49bbaf71a485
AlexandrTurov <alexandr.turov@mail.ru>
2024-09-03 16:54:45 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import paths from "vite-tsconfig-paths";
+
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [
+    react({ plugins: [] }),
+    paths({ root: '../' })
+  ],
+});
